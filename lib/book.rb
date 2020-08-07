5415f5db10d47bc4b6d715569429810e500654a4
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-07 21:42:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,11 +9,19 @@ class Book
   end
 
   def author= (author)
-  @author = author
+    @author = author
   end
   
   def author
-  @author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
   end
   
 end
